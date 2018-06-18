d634d9e499724294951cb5c83cb6c92671f4d416
mansig <mansig16551@gmail.com>
2018-06-18 14:40:55 +0530

    sol 3 added

---
+++ Assignment1/3.java
@@ -0,0 +1,11 @@
+public class me{
+	public static void main(String[] args){
+ 		int a=10;
+		int d;
+                float b=12.5F;
+		String c="java programming";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+}
+}
