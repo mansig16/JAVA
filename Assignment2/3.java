a5dc2ec33f1e53b03a8f020a8924e11a9f366ba6
mansig <mansig16551@gmail.com>
2018-06-19 19:55:17 +0530

    assign 3 added

---
+++ Assignment2/3.java
@@ -0,0 +1,45 @@
+public class day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=3;
+		if(i==1)
+		{
+			System.out.println("sunday");
+		}
+		else if(i==2)
+		{
+			System.out.println("monday");
+		}
+		else if(i==3)
+		{
+			System.out.println("tuesday");
+		}
+		else if(i==4)
+		{
+			System.out.println("wednesday");
+		}
+		else if(i==5)
+		{
+			System.out.println("thursday");
+		}
+		else if(i==6)
+		{
+			System.out.println("friday");
+		}
+		else if(i==7)
+		{
+			System.out.println("saturday");
+		}
+		else if(i==0)
+		{
+			System.out.println("saturday");
+		}
+		else
+		{
+			System.out.println("error");
+		}
+
+	}
+
+}
