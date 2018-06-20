95dc30017af9ac3e20da2f8aba971f032197eb94
mansig <mansig16551@gmail.com>
2018-06-20 20:29:04 +0530

    sol 1

---
+++ Assignment3/1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ques1 {
+	static Scanner keyboard=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("enter two words and two integers");
+		keyboard.nextInt();
+		keyboard.nextInt();
+		keyboard.next();
+		keyboard.next();
+		
+	}
+
+}
