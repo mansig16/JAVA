126f01f973e8ea1db3a52c1017d380c1e269b822
mansig <mansig16551@gmail.com>
2018-06-21 21:20:46 +0530

    sol 20

---
+++ Assignment3/20.java
@@ -0,0 +1,12 @@
+public class ques8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x;
+		for(x=-10;x<=10;x=x+0.5){
+			System.out.println(x);
+		}
+
+	}
+
+}
