58bdada71f2c45d01c65737f96224eb89c96e962
mansig <mansig16551@gmail.com>
2018-06-21 21:16:33 +0530

    sol 9

---
+++ Assignment3/9.java
@@ -0,0 +1,46 @@
+package practise;
+
+public class ques11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Comparing \"axe\" with \"dog\" produces ");
+		int i = "axe".compareTo("dog");
+		System.out.println(i);
+		
+		System.out.print("Comparing \"apple\" with \"litchi\" produces ");
+		System.out.println( "apple".compareTo("litchi") );
+		
+		System.out.print("Comparing \"apple\" with \"apple\" produces ");
+		System.out.println( "apple".compareTo("apple") );
+		
+		System.out.print("Comparing \"mansi\" with \"dimple\" produces ");
+		System.out.println( "mansi".compareTo("dimple") );
+		
+		System.out.print("Comparing \"ram\" with \"yup\" produces ");
+		System.out.println( "ram".compareTo("yup") );
+
+		System.out.print("Comparing \"mango\" with \"fruit\" produces ");
+		System.out.println( "mango".compareTo("fruit") );
+		
+		System.out.print("Comparing \"fruit\" with \"fruit\" produces ");
+		System.out.println( "fruit".compareTo("fruit") );
+		
+		System.out.print("Comparing \"bee\" with \"app\" produces ");
+		System.out.println( "bee".compareTo("app") );
+		
+		System.out.print("Comparing \"name\" with \"owl\" produces ");
+		System.out.println( "name".compareTo("owl") );
+		
+		System.out.print("Comparing \"ab1\" with \"ab2\" produces ");
+		System.out.println( "ab1".compareTo("ab2") );
+		
+		System.out.print("Comparing \"vc2\" with \"bb\" produces ");
+		System.out.println( "vc2".compareTo("bb") );
+		
+		System.out.print("Comparing \"mn1\" with \"gm\" produces ");
+		System.out.println( "mn1".compareTo("gm") );
+
+	}
+
+}
