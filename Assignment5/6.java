e82da591a695555606f04ecd319565e92c8dceef
mansig <mansig16551@gmail.com>
2018-06-23 22:48:59 +0530

    sol 6

---
+++ Assignment5/6.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ques3 {
+	static Scanner ui=new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the string");
+		String s1=ui.next();
+		System.out.println(s1.charAt(4));
+	}
+
+}
